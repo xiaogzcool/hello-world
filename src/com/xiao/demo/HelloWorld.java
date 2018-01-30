89bf8bb4b7359718492dfa2fbfefa6b84c54eb11
comtexiao <comtexiao@qq.com>
2018-01-30 18:08:30 +0800

    新增一条输出语句

--- src/com/xiao/demo/HelloWorld.java
+++ src/com/xiao/demo/HelloWorld.java
@@ -6,5 +6,6 @@ package com.xiao.demo;
 public class HelloWorld {
     public static void main(String[] args){
         System.out.println("Hello World! 你好世界");
+        System.out.println("hahh");
     }
 }
