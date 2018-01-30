f589b8c75cd564c00e49429c4814f04ff82a884b
comtexiao <comtexiao@qq.com>
2018-01-30 16:56:47 +0800

    Initial commit

---
+++ src/com/xiao/demo/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.xiao.demo;
+
+/**
+ * 你好世界,helloworld类
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello World! 你好世界");
+    }
+}
