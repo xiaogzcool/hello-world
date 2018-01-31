779c344d60d6be251ca3dfcda25b37d042bec3c4
comtexiao <comtexiao@qq.com>
2018-01-31 13:31:18 +0800

    新增一条输出语句

--- src/com/xiao/demo/HelloWorld.java
+++ src/com/xiao/demo/HelloWorld.java
@@ -7,5 +7,6 @@ public class HelloWorld {
     public static void main(String[] args){
         System.out.println("Hello World! 你好世界");
         System.out.println("hahh");
+        System.out.println("-----------YouMeek.com-----------值=" + args+"," + "当前类=HelloWorld.main()");
     }
 }
